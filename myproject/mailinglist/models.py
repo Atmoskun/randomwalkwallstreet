922fa16afcfa6353692a1fe15a75e0044afb19e4
Atmoskun <grogorx@gmail.com>
2025-10-31 18:24:05 -0400

    Installed Django and created a new Django project and app for the mailing list functionality.

---
+++ myproject/mailinglist/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# mailinglist/models.py
+from django.db import models
+
+class Submission(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.username
