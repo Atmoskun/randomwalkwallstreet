ae9120c7c150a243e4ae3a79966841ca8b2d78d2
Atmoskun <grogorx@gmail.com>
2025-11-07 22:26:40 -0500

    modified:   Randomwalk/db.sqlite3 	modified:   Randomwalk/mailinglist/__pycache__/models.cpython-313.pyc 	modified:   Randomwalk/mailinglist/__pycache__/urls.cpython-313.pyc 	modified:   Randomwalk/mailinglist/__pycache__/views.cpython-313.pyc 	new file:   Randomwalk/mailinglist/templates/mailinglist/earnings.html 	modified:   Randomwalk/mailinglist/templates/mailinglist/index.html 	modified:   Randomwalk/mailinglist/urls.py 	modified:   Randomwalk/mailinglist/views.py

--- Randomwalk/mailinglist/urls.py
+++ Randomwalk/mailinglist/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
     path('earnings/', views.earnings, name='earnings'),
+    path('', views.earnings, name='earnings'),
+
 ]
