a9f8bdb04f73e3f94c12dbdd6f213ea981dcffde
Atmoskun <grogorx@gmail.com>
2025-11-07 22:51:28 -0500

    Changes to be committed: 	modified:   Randomwalk/mailinglist/__pycache__/urls.cpython-313.pyc 	modified:   Randomwalk/mailinglist/__pycache__/views.cpython-313.pyc 	modified:   Randomwalk/mailinglist/urls.py 	modified:   Randomwalk/mailinglist/views.py

--- Randomwalk/mailinglist/urls.py
+++ Randomwalk/mailinglist/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.earnings, name='earnings'),
+    path('earnings/', views.earnings, name='earnings'),
 ]
