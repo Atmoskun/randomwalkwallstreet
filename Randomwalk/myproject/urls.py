3959cdfce690e0107ff973d786a299681391cf62
888peter888 <pyang6777@gmail.com>
2025-11-12 01:27:05 -0500

    Update

--- Randomwalk/myproject/urls.py
+++ Randomwalk/myproject/urls.py
@@ -14,14 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', include('mailinglist.urls')),   # coworker's frontend routes
+    # 1. the homepage
+    path('', include('mailinglist.urls')),
 
+    # 2. Your app is added under the 'analysis/' path
+    # Your webpage will be at: /analysis/
+    # Your health API will be at: /analysis/api/health
+    path('analysis/', include('api.urls')),
 ]
-
-
