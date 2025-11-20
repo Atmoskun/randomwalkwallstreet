73a231a8520f6114fe1e396ef61cece7055cbe77
Atmoskun <grogorx@gmail.com>
2025-11-19 23:06:32 -0500

    modified:   Randomwalk/myproject/settings.py

--- Randomwalk/myproject/settings.py
+++ Randomwalk/myproject/settings.py
@@ -4,7 +4,7 @@ Django settings for myproject project.
 
 from pathlib import Path
 import os
-# import dj_database_url
+import dj_database_url
 # from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,14 +122,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # DATABASE
 # ---
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
-# Use PostgreSQL for Render deployment if URL is present
-# if 'DATABASE_URL' in os.environ:
-#     DATABASES['default'] = dj_database_url.parse(os.environ.get('DATABASE_URL'))
 
 
 # Password validation (Standard)
