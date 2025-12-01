9b882d60ed81283121181ee4ec6a2aa3315e5839
888peter888 <pyang6777@gmail.com>
2025-11-30 20:23:43 -0500

    Update urls.py

--- Randomwalk/api/urls.py
+++ Randomwalk/api/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     # Main page view (default path for the app)
     path('', views.quarterly_selection_view, name='quarterly_selection'),
-    
+
+    # Analytics / Tracking endpoint
+    path('tracking/api/update-time/', views.update_visit_time, name='update_time'),
 ]
